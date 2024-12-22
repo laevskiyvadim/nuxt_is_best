28ea4142a440f902d10d059e8f4f3b62ca0479f8
laevskiyvadim <laevskiy.vadim@mail.ru>
2024-12-22 13:42:49 -0800

    about author

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,34 @@
 <template>
   <div class="main-page">
     <HeaderMenu />
-    <div>about author</div>
+    <div class="center">
+      <h1>Автор: Лаевский Вадим</h1>
+      <h4>Учебное заведение: Финаносовый университет</h4>
+      <h4>Группа: ПИ 22-2в</h4>
+      <div>Контактные данные: 222734@edu.fa.ru
+        <span><b>для оперативной связи следите за моим расписанием</b></span>
+      </div>
+      <br>
+      <h3>Список технологий и опыт</h3>
+      <ol>
+        <li>Java - с 1 сентября 2024г.</li>
+        <li>JavaScript - уже и не помню когда начал</li>
+        <li>Spring Boot - первый раз, можно считать, что с сентября</li>
+        <li>Nuxt - первый раз</li>
+      </ol>
+      <div>Даты начала и окончания работ: 20.11.2024 - 10.12.2024</div>
+    </div>
   </div>
 </template>
+<style>
+.center {
+  text-align: center;
+}
+
+ol {
+  padding: 0 0 0 40%;
+  li {
+    text-align: left;
+  }
+}
+</style>
